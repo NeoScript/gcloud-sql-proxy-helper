eb11142b713e7ea00e379f0b80015646c29ff1e0
neoscript <neoscripting@gmail.com>
2025-04-26 18:40:40 -0500

    Add support for colors

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,40 @@ use std::io::BufReader;
 use std::process::Command;
 use std::process::Stdio;
 
+use owo_colors::OwoColorize;
+
 fn main() {
-    let mut child = Command::new("/home/nasir/cloud-sql-proxy")
-        .args(["badg-r:us-central1:staging", "--port", "9000"])
+    let path = "/Users/nasir/cloud-sql-proxy";
+    let instance = "badg-r:us-central1:staging";
+    let port = "9000";
+    start_proxy(path, instance, port);
+}
+
+fn start_proxy(path: &str, instance: &str, port: &str) {
+    let mut child = Command::new(path)
+        .args([instance, "--port", port])
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()
-        .expect("Failed to start proxy");
+        .expect("cloud-sql-proxy should have started:");
 
     if let Some(stdout) = child.stdout.take() {
         std::thread::spawn(move || {
             let reader = BufReader::new(stdout);
             for line in reader.lines() {
                 match line {
-                    Ok(l) => println!("{}", l),
-                    Err(e) => eprintln!("stdout read error: {}", e),
+                    Ok(l) => println!("info: {}", l.green()),
+                    Err(e) => eprintln!("error: {}", e.red()),
                 }
             }
         });
     }
 
-    child.wait_with_output().expect("should do something");
+    child.wait_with_output().expect("proxy should have run");
 }
+
+// Things that I want to support
+// check for settings file
+// if it exists then we should load them in
+// if not then lets create a config file in a default .config place
+//
