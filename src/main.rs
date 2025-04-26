9a0b301252284dba6ce377d9d27fd311efcc9588
neoscript <neoscripting@gmail.com>
2025-04-26 16:56:03 -0500

    Get the basic output working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::BufRead;
+use std::io::BufReader;
+use std::process::Command;
+use std::process::Stdio;
+
 fn main() {
-    println!("Hello, world!");
+    let mut child = Command::new("/home/nasir/cloud-sql-proxy")
+        .args(["badg-r:us-central1:staging", "--port", "9000"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("Failed to start proxy");
+
+    if let Some(stdout) = child.stdout.take() {
+        std::thread::spawn(move || {
+            let reader = BufReader::new(stdout);
+            for line in reader.lines() {
+                match line {
+                    Ok(l) => println!("{}", l),
+                    Err(e) => eprintln!("stdout read error: {}", e),
+                }
+            }
+        });
+    }
+
+    child.wait_with_output().expect("should do something");
 }
